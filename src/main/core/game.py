e81b5998b77bdfbe46f8e5eb004066e098039479
Sylvain Dazy <sylvain.dazy@hotmail.com>
2024-01-26 17:24:39 +0100

    Insert coins

---
+++ src/main/core/game.py
@@ -0,0 +1,21 @@
+class Game:
+    def __init__(self, rows: int = 6, cols: int = 7, colors: tuple[str, str] = ("R", "Y")):
+        self.grid = [["" for _ in range(cols)] for _ in range(rows)]
+        self.color = colors
+        self.current_player = 0
+
+    def insert_coin(self, col: int):
+        row = len(self.grid) - 1
+        while self.is_not_free(row, col):
+            row -= 1
+        self.grid[row][col] = self.get_current_color()
+        self.next_player()
+
+    def is_not_free(self, row: int, col: int):
+        return self.grid[row][col] != ""
+
+    def next_player(self):
+        self.current_player = (self.current_player + 1) % len(self.color)
+
+    def get_current_color(self) -> str:
+        return self.color[self.current_player]
---
+++ src/test/test_game.py
@@ -0,0 +1,62 @@
+import pytest
+
+from src.main.core.game import Game
+
+
+@pytest.fixture
+def game():
+    return Game(6, 7, ("R", "Y"))
+
+
+def test_new_grid_is_empty(game):
+    assert game.grid == [["", "", "", "", "", "", ""],
+                         ["", "", "", "", "", "", ""],
+                         ["", "", "", "", "", "", ""],
+                         ["", "", "", "", "", "", ""],
+                         ["", "", "", "", "", "", ""],
+                         ["", "", "", "", "", "", ""]]
+
+
+def test_insert_coin(game):
+    game.insert_coin(0)
+    assert game.grid == [["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["R", "", "", "", "", "", ""]]
+
+
+def test_insert_2_coins_in_same_column(game):
+    game.insert_coin(0)
+    game.insert_coin(0)
+    assert game.grid == [["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["Y", "", "", "", "", "", ""],
+                         ["R", "", "", "", "", "", ""]]
+
+
+def test_after_one_turn_restart_with_first_player(game):
+    game.insert_coin(0)
+    game.insert_coin(0)
+    game.insert_coin(3)
+    assert game.grid == [["",  "", "", "",  "", "", ""],
+                         ["",  "", "", "",  "", "", ""],
+                         ["",  "", "", "",  "", "", ""],
+                         ["",  "", "", "",  "", "", ""],
+                         ["Y", "", "", "",  "", "", ""],
+                         ["R", "", "", "R", "", "", ""]]
+
+
+def test_insert_3_times_in_same_col(game):
+    game.insert_coin(0)
+    game.insert_coin(0)
+    game.insert_coin(0)
+    assert game.grid == [["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["",  "", "", "", "", "", ""],
+                         ["R", "", "", "", "", "", ""],
+                         ["Y", "", "", "", "", "", ""],
+                         ["R", "", "", "", "", "", ""]]
